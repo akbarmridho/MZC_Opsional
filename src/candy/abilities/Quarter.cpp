25f04b6f916cfa5db8607dd65740fa1eb0303331
arsaizdihar <arsadihar@gmail.com>
2023-03-11 12:13:09 +0700

    Update ability

--- src/candy/abilities/Quarter.cpp
+++ src/candy/abilities/Quarter.cpp
@@ -1,24 +1,25 @@
 #include "Quarter.hpp"
 #include <iostream>
 
-Quarter::Quarter(PointManager *pm) : AbilityCard("Quarter") {
+Quarter::Quarter(PointManager *pm) : AbilityCard("Quarter")
+{
   pointManager = pm;
 }
 
 void Quarter::use()
 {
-  notUsedOrThrow();
-  activeOrThrow();
   std::cout << *getOwner();
 
-  if (pointManager->getCurrentRewardPoint() >= 4) {
+  if (pointManager->getCurrentRewardPoint() >= 4)
+  {
     std::cout << " melakukan QUARTER! Poin hadiah turun " << pointManager->getCurrentRewardPoint() << " menjadi ";
     pointManager->multiplyReward(0.25);
     std::cout << pointManager->getCurrentRewardPoint() << "!\n";
-  } else {
-    std::cout << 
-    " melakukan QUARTER! Sayangnya poin hadiah sudah bernilai kurang dari 4. Poin hadiah tidak berubah.. Giliran dilanjut\n";
+  }
+  else
+  {
+    std::cout << " melakukan QUARTER! Sayangnya poin hadiah sudah bernilai kurang dari 4. Poin hadiah tidak berubah.. Giliran dilanjut\n";
   }
 
-  setUsed();
+  getStatus().setUsed();
 }
--- src/candy/abilities/Reverse.cpp
+++ src/candy/abilities/Reverse.cpp
@@ -1,16 +1,15 @@
 #include "Reverse.hpp"
 #include <iostream>
 
-Reverse::Reverse(RoundManager *rm) : AbilityCard("Reverse") {
+Reverse::Reverse(RoundManager *rm) : AbilityCard("Reverse")
+{
   roundManager = rm;
 }
 
 void Reverse::use()
 {
-  notUsedOrThrow();
-  activeOrThrow();
   std::cout << *getOwner() << " melakukan REVERSE!\n";
   roundManager->reversePlayer();
 
-  setUsed();
+  getStatus().setUsed();
 }
